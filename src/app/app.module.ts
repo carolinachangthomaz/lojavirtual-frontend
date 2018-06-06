4745dc69a740354961b1dcf18e2c33fbff88a1e0
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-06 20:11:46 -0300

    Começando a implementar a autenticação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptor } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
 
 
 @NgModule({
@@ -29,6 +30,7 @@ import { ErrorInterceptor } from '../interceptors/error-interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
+    AuthService,
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
   
