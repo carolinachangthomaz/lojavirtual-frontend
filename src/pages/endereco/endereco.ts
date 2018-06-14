9d4b08ade42c889187a8b8ac9ef7dde3dd6a0010
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-13 21:14:06 -0300

    Buscando Endereços no BD e Refatorando cliente service

--- src/pages/endereco/endereco.ts
+++ src/pages/endereco/endereco.ts
@@ -1,6 +1,8 @@
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 /**
  * Generated class for the EnderecoPage page.
@@ -18,44 +20,26 @@ export class EnderecoPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+               public navParams: NavParams,
+               public storage: StorageService,
+               public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Hum",
-        numero: "23",
-        complemento: "Apto 4",
-        bairro: "Centro",
-        cep: "232432545",
-        cidade: {
-          id: "1",
-          nome: "São Paulo",
-          estado: {
-            id: "1",
-            nome: "SP"
-          }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua dois",
-        numero: "454",
-        complemento: "casa",
-        bairro: "Centro",
-        cep: "77676777",
-        cidade: {
-          id: "2",
-          nome: "Uberlândia",
-          estado: {
-            id: "2",
-            nome: "Minas"
-          }
-        }
-      }
-     ]
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.items = response['enderecos'];
+      }, error => {
+           if(error.status == 403){
+             this.navCtrl.setRoot('HomePage');
+           }
+      })
+    }else{
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
